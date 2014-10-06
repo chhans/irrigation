4a7663458a6ce1ec766933fe5f8328e0b92e4629
Christian A H Hansen <christian.ahh@gmail.com>
2014-10-06 10:06:11 +0200

    Updated sprinkler

---
+++ Sprinkler/src/sprinkler/impl/SprinklerImpl.java
@@ -0,0 +1,19 @@
+package sprinkler.impl;
+
+import sprinkler.Sprinkler;
+
+public class SprinklerImpl implements Sprinkler {
+
+	@Override
+	public void startSprinkler() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void stopSprinkler() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
